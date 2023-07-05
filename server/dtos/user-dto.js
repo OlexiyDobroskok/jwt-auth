86a78f43ff856b2d1ef70573067c3dec3f072c31
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-07-05 21:04:17 +0300

    Deleted files after destructuring

--- server/dtos/user-dto.js
+++
@@ -1,9 +0,0 @@
-exports.getUserDto = (model) => {
-  const { _id, email, username, isActivated } = model;
-  return {
-    id: _id,
-    email,
-    username,
-    isActivated,
-  };
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-const { PORT } = require("./utils/config");
-const { info } = require("./utils/logger");
-
-app.listen(PORT, () => info(`Server started on PORT ${PORT}`));
