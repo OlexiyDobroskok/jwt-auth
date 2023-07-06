c338c02450f2112ff038294a0fd12f53f7d29f9f
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-07-06 09:46:19 +0300

    Create a refresh feature

---
+++ src/features/authentication/refresh/model/refreshThunk.ts
@@ -0,0 +1,35 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { type ApiException, handleApiError } from "shared/api";
+import { clearSession, createSession } from "entities/session";
+import { createUser, removeUser } from "entities/user";
+import { refresh } from "../api/endpoints";
+
+export const refreshThunk = createAsyncThunk<
+  Token,
+  void,
+  { dispatch: AppDispatch; rejectValue: ApiException }
+>("user/refreshThunk", async (_, { dispatch, rejectWithValue }) => {
+  try {
+    const { userDto, accessToken } = await refresh();
+    if (accessToken) {
+      const { id, email, userName, isActivated, date } = userDto;
+      dispatch(
+        createSession({
+          userId: id,
+          isActivated,
+          accessToken,
+        })
+      );
+      dispatch(createUser({ id, email, userName, registrationDate: date }));
+    }
+    return accessToken;
+  } catch (error) {
+    const apiException = handleApiError(error);
+    if (apiException.status === 401) {
+      dispatch(clearSession());
+      dispatch(removeUser());
+    }
+
+    return rejectWithValue(apiException);
+  }
+});
